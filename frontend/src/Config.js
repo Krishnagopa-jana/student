b54f8a10842457ed1df5f24e68f672be3d64fdf4
Krishnagopa-jana <krishnagopaljana75@gmail.com>
2025-06-17 22:05:21 +0530

    Update Config.js
    change base url to render url

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:5000"; // Yahan aap production URL bhi use kar sakte ho
+const BASE_URL = "https://student-8nji.onrender.com"; // Yahan aap production URL bhi use kar sakte ho
 
 export default BASE_URL;
