8b6af85f3f740afd290c8bc2c5eb500a87354855
Krishnagopa-jana <krishnagopaljana75@gmail.com>
2025-06-17 22:21:36 +0530

    Update Config.js

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://student-8nji.onrender.com"; // Yahan aap production URL bhi use kar sakte ho
+const BASE_URL = "https://student-backend-a759.onrender.com"; // Yahan aap production URL bhi use kar sakte ho
 
 export default BASE_URL;
